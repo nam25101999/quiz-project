0dd1917ecf9baf70bd1f3fd7b8fd9e451bb48c47
Nam Ne <nguyennam25101999@gmail.com>
2024-11-19 03:19:03 +0700

    Cập nhật mã nguồn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactjjjjjjjjjjjjjj
+          Learn Reactjjjjjjjjjjjjjj hiểu bài chưa
         </a>
       </header>
     </div>
