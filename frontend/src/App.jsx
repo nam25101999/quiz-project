168c0af457201801f938c2b10512efa986a4c957
Nam Ne <nguyennam25101999@gmail.com>
2024-11-22 02:43:49 +0700

    cập nhật css cho app.js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import ExamList from './components/ExamList';
+import CreateExam from './components/CreateExam';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/login">Đăng nhập</Link> | <Link to="/register">Đăng ký</Link>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <main>
+          <Link to="/create">Tao Bai Tap </Link> 
+          <Link to="/exam-list">Danh Sach Bai Tap </Link>
+          <Routes>
+            <Route path="/create" element={<CreateExam />} />
+            <Route path="/exam-list" element={<ExamList />} />
+          </Routes>
+        </main>
+        
+      </div>
+    </Router>
+    
+  );
+};
+
+export default App;
